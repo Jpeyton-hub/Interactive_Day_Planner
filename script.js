8ea16e33d89e40fac016b4885acaa47ee0242b9b
Jpeyton <jpwalk319@gmail.com>
2021-01-15 12:01:39 -0700

    added event listeners for the btn clicks so they change the image in the button

--- script.js
+++ script.js
@@ -9,10 +9,25 @@ for (let i = 0; i < descArr.length; i++) {
     if (descArr[i].dataset.time == currentHour) {
         descArr[i].classList.add('present')
     }
-    else if (descArr[i] > currentHour) {
+    else if (parseInt(descArr[i].dataset.time) > currentHour) {
         descArr[i].classList.add('future')
     }
-    else if (descArr[i] < currentHour) {
+    else if (parseInt(descArr[i].dataset.time) < currentHour) {
+        descArr[i].classList.remove('future')
+        descArr[i].classList.remove('present')
         descArr[i].classList.add('past')
     }
 }
+
+$('.saveBtn').on('click', function() {
+    $(this).toggleClass('activeBtn');
+    $(this).children()[0].setAttribute('class', 'fa fa-unlock');
+    $(this).toggleClass('saveBtn');
+    $('.activeBtn').one('click', function() {
+        $(this).children()[0].setAttribute('class', 'fa fa-lock');
+        $(this).toggleClass('activeBtn');
+        $(this).toggleClass('saveBtn');
+    });
+});
+
+
