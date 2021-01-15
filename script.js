13c264c1792a5ff053ac4a17f3248dc135ef85c9
Jpeyton <jpwalk319@gmail.com>
2021-01-15 09:46:13 -0700

    added js to change colors of description divs with hours

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// setting current day in the header
 $('#currentDay').text(dayjs().format('dddd, MMMM D'));
-const currentHour = dayjs().format('ha');
 
+// declaring variables for time comparison
+const currentHour = dayjs().format('H');
+const descArr = $('.description')
+
+for (let i = 0; i < descArr.length; i++) {
+    if (descArr[i].dataset.time == currentHour) {
+        descArr[i].classList.add('present')
+    }
+    else if (descArr[i] > currentHour) {
+        descArr[i].classList.add('future')
+    }
+    else if (descArr[i] < currentHour) {
+        descArr[i].classList.add('past')
+    }
+}
